ee40a76877b6254ba28404e343f336220fce4282
ZeroBone <zerobone21@gmail.com>
2021-11-14 01:22:19 +0100

    Implemented the `SameDiv(x, y_1, ..., y_n)` function

--- src/presmondec.py
+++ src/presmondec.py
@@ -1,7 +1,7 @@
 from z3 import *
 
 
-def compute_bound(f) -> int:
+def _compute_bound(f) -> int:
 
     visited = set()
 
@@ -69,29 +69,89 @@ def congruent(lhs, rhs, modulo):
     return (lhs - rhs) % modulo == 0
 
 
-def same_div(x_1, x_2, x, phi):
+# def _substitute_var(f, x, y):
+#
+#     if is_const(f) and f.decl().kind() == Z3_OP_UNINTERPRETED and eq(f, x):
+#         print("Detected variable x", f)
+#         return y
+#
+#     if len(f.children()) == 0:
+#         return f
+#
+#     return f.decl()([_substitute_var(child, x, y) for child in f.children()])
+
+
+def _same_div_transform_constraint(x_1, x_2, x, div_constraint):
+
+    lhs, rhs, modulo = div_constraint
+
+    lhs_x1 = substitute(lhs, (x, x_1))
+    rhs_x1 = substitute(rhs, (x, x_1))
+
+    lhs_x2 = substitute(lhs, (x, x_2))
+    rhs_x2 = substitute(rhs, (x, x_2))
+
+    return congruent(lhs_x1, rhs_x1, modulo) == congruent(lhs_x2, rhs_x2, modulo)
+
+
+def _same_div(x_1, x_2, x, phi):
+
+    visited = set()
+    div_constraints = set()
 
     def detect_div_constraints(node):
-        if node.decl().kind() == Z3_OP_EQ:
-            pass
+        if node.decl().kind() == Z3_OP_EQ and \
+                node.arg(0).decl().kind() == Z3_OP_MOD and \
+                node.arg(0).arg(0).decl().kind() == Z3_OP_SUB and \
+                node.arg(0).arg(1).decl().kind() == Z3_OP_ANUM and \
+                node.arg(1).decl().kind() == Z3_OP_ANUM and \
+                node.arg(1).as_long() == 0:
+            # detected the ast root of a divisibility constraint
 
-    # TODO
-    pass
+            subtr = node.arg(0).arg(0)
+            assert subtr.decl().kind() == Z3_OP_SUB
+
+            lhs = subtr.arg(0)
+            rhs = subtr.arg(1)
+
+            modulo = node.arg(0).arg(1)
+
+            div_constraints.add((lhs, rhs, modulo))
+            return
+
+        for child in node.children():
+
+            if child in visited:
+                continue
+
+            visited.add(child)
+
+            detect_div_constraints(child)
+
+    visited.add(phi)
+    detect_div_constraints(phi)
+
+    print("Divisibility constraints found:", div_constraints)
+
+    return And([
+        _same_div_transform_constraint(x_1, x_2, x, c)
+        for c in div_constraints
+    ])
 
 
 def monadic_decomposable(f, x) -> bool:
 
-    b = compute_bound(f)
+    b = _compute_bound(f)
 
     print("B =", b)
 
-    # TODO
+    x_1, x_2 = Ints("x_1 x_2")
 
-    print(f.num_args())
-    print(f.arg(0))
-    print(f.children())
-    print(f.arg(5).children())
-    print(f.arg(0).arg(0))
+    s = _same_div(x_1, x_2, x, f)
+
+    print(s)
+
+    # TODO
 
     return True
 
@@ -108,6 +168,8 @@ if __name__ == "__main__":
         z < 5,
         congruent(x, y, 2)
     ])
+
+    print(phi)
 
     if monadic_decomposable(phi, x):
         print("Monadically decomposable")
