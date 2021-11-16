3d85aacb653669580068c57ecaaf931bece0a2b7
ZeroBone <zerobone21@gmail.com>
2021-11-16 01:08:57 +0100

    Improved tests, made it possible to run any test without the second decomposability checker being tested because it seems to take a very long time to run it

--- src/tests.py
+++ src/tests.py
@@ -4,7 +4,8 @@ from mondec import mondec
 from utils import get_formula_variables
 
 
-def test(phi, *decomposable_on):
+def test(phi, *decomposable_on, fast_version=False) -> bool:
+    test_pass = True
     phi_vars = [v.unwrap() for v in get_formula_variables(phi)]
 
     if len(decomposable_on) == 0:
@@ -13,29 +14,39 @@ def test(phi, *decomposable_on):
             if monadic_decomposable(phi, v):
                 print("❌ A non-decomposable formula %s has been considered "
                       "decomposable by monadic_decomposable() when decomposing on variable %s" % (phi, v))
+                test_pass = False
             else:
                 print("✔ monadic_decomposable(phi, %s) = False" % v)
+
+            if fast_version:
+                continue
 
             if monadic_decomposable_without_bound(phi, v):
                 print("❌ A non-decomposable formula %s has been considered "
                       "decomposable by monadic_decomposable_without_bound() "
                       "when decomposing on variable %s" % (phi, v))
+                test_pass = False
             else:
                 print("✔ monadic_decomposable_without_bound(phi, %s) = False" % v)
 
-        return
+        return test_pass
 
     for v in decomposable_on:
         if not monadic_decomposable(phi, v):
-            print("A decomposable formula %s has been considered "
+            print("❌ A decomposable formula %s has been considered "
                   "non-decomposable by monadic_decomposable() when decomposing on variable %s" % (phi, v))
+            test_pass = False
         else:
             print("✔ monadic_decomposable(phi, %s) = True" % v)
 
+        if fast_version:
+            continue
+
         if not monadic_decomposable_without_bound(phi, v):
-            print("A decomposable formula %s has been considered "
+            print("❌ A decomposable formula %s has been considered "
                   "non-decomposable by monadic_decomposable_without_bound() "
                   "when decomposing on variable %s" % (phi, v))
+            test_pass = False
         else:
             print("✔ monadic_decomposable_without_bound(phi, %s) = True" % v)
 
@@ -49,30 +60,32 @@ def test(phi, *decomposable_on):
     print("General purpose monadic decomposability checker terminated.")
     print("=" * 30)
 
+    return test_pass
+
 
 def print_test_start(i: int):
     print("==================== [TEST %d] ====================" % i)
 
 
-def test_1():
+def test_1() -> bool:
     x, y = Ints("x y")
     phi = And(x >= 0, y >= 0, x == y)
-    test(phi)
+    return test(phi)
 
 
-def test_2():
+def test_2() -> bool:
     x, y = Ints("x y")
     phi = And(x >= 0, y >= 0, x <= y)
-    test(phi)
+    return test(phi)
 
 
-def test_3():
+def test_3() -> bool:
     x, y = Ints("x y")
     phi = And(x >= 0, y >= 0, x >= y)
-    test(phi)
+    return test(phi)
 
 
-def test_4():
+def test_4() -> bool:
     x, y, z = Ints("x y z")
 
     phi = And([
@@ -84,18 +97,23 @@ def test_4():
         congruent(x, y, 2)
     ])
 
-    test(phi, x, y, z)
+    return test(phi, x, y, z, fast_version=True)
 
 
 def run_tests():
+
+    tests_passed = True
+
     print_test_start(1)
-    test_1()
+    tests_passed = tests_passed and test_1()
     print_test_start(2)
-    test_2()
+    tests_passed = tests_passed and test_2()
     print_test_start(3)
-    test_3()
+    tests_passed = tests_passed and test_3()
     print_test_start(4)
-    test_4()
+    tests_passed = tests_passed and test_4()
+
+    print("Test result: %s" % "PASS" if tests_passed else "FAIL")
 
 
 if __name__ == "__main__":
